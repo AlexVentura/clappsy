587ebbce76fa174bf7d6b6da10e8700a48fd1c04
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2016-08-09 01:55:05 -0500

    Changing the description in comments.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # Configuracion para Devise
+  # Devise manual configuration
   config.action_mailer.default_url_options = { host: 'IP', port: 3000 }
 end
