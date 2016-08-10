19460c6ffff14725edd61034efd9b9d9308aa9be
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2016-08-10 01:00:14 -0500

    Applying migrations to link User and Profile.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810055052) do
+ActiveRecord::Schema.define(version: 20160810055836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "people", force: :cascade do |t|
+  create_table "profiles", force: :cascade do |t|
     t.string   "name"
     t.string   "second_name"
     t.string   "last_name"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20160810055052) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["user_id"], name: "index_people_on_user_id", using: :btree
+    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,5 +51,5 @@ ActiveRecord::Schema.define(version: 20160810055052) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "people", "users"
+  add_foreign_key "profiles", "users"
 end
