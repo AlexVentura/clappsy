dbe5414c760fa948f13489a30548e9e738b21292
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2016-08-11 01:00:16 -0500

    Joining the Profile and User models in just one, User.

---
+++ db/migrate/20160811051619_add_fields_to_users.rb
@@ -0,0 +1,12 @@
+class AddFieldsToUsers < ActiveRecord::Migration[5.0]
+	def change
+		add_column :users, :first_name, :string
+		add_column :users, :second_name, :string
+		add_column :users, :last_name, :string
+		add_column :users, :birthday, :date
+		add_column :users, :adress, :string
+		add_column :users, :city, :string
+		add_column :users, :state, :string
+		add_column :users, :role, :string
+	end
+end
