792efa899ee35f4b5e36832e59487a71e2750130
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2016-08-11 20:13:17 -0500

    Styling up the homepage.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require bootstrap-sprockets
+//= require bootstrap-datetimepicker
+//= require moment
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
